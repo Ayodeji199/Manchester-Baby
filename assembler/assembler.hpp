9423ab17959f706af947e15cb689af1bd421d3a4
Max Kelly <maxwill.kelly@gmail.com>
2019-11-21 19:14:40 +0000

    Resolves includes in assembler

--- assembler/assembler.hpp
+++ assembler/assembler.hpp
@@ -1,8 +1,10 @@
-#include <iostream>
 
 #include "../file/file.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 // Seperates a line into different components
-int splitLine(string line, vector<string> token);
+int splitLine(std::string line, std::vector<std::string> token);
