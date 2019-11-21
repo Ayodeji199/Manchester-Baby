5fd04a4919522bc14dcff3265afd2569496ea4ae
Max Kelly <maxwill.kelly@gmail.com>
2019-11-21 17:01:53 +0000

    Adds a symbol table data structure

---
+++ assembler/symbolTable.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <map>
+#include <unordered_map>
+
+// Stores memory locations of user defined variables
+class SymbolTable
+{
+private:
+    // Stores the symbols as a map which is essentially a hash table
+    std::map<std::string, int> table;
+    // Stores the number of items stored in the table and the number of memory locations we've used
+    int num;
+
+public:
+    // Constructor
+    SymbolTable();
+    // Stores a variable in the symbol table if it hasn't already been stored and assigns it a memory location
+    int store(std::string var);
+    // Returns the memory location of a variable
+    int get(std::string var);
+};
