ba22620568649480d3037dd229b34450b8d5432c
Max Kelly <maxwill.kelly@gmail.com>
2019-11-22 13:13:16 +0000

    Decimal and binary conversion
    Co-authored-by: Max Fyall (Student) <m.fyall@dundee.ac.uk>

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -1,1 +1,99 @@
 #include "converter.hpp"
+#include <iostream>
+#include <locale>
+#include <algorithm>
+
+using namespace std;
+
+int binaryToDecimal(string binary)
+{
+      // error checking, check if the string is empty, check if the string is in 8 bit binary format
+        if (binary.empty())
+        {
+            // print an error message if any of the above conditions are true
+            cout << "INVALID INPUT - PLEASE TRY AGAIN" << endl;
+            cout << endl;
+            return 999; //  return error code;
+        }
+
+        // initialise variables for error check algorithm
+        int i = 0;
+        string tempString;
+
+        // while loop runs thorugh the inputed string checking if any of the elements are not a 1 or a 0
+        while (binary[i] != '\0')
+        {
+            // set the temporary variable to the element in the string
+            tempString = binary[i];
+
+            // use the compare function to check if the element is a 0 (i.e. does it return 0)
+            if (tempString.compare("0") != 0)
+            {
+                // if the element is not a 0 then it must be a 1, use compare function to check
+                if (tempString.compare("1") != 0)
+                {
+                    // if the element is not a 1 then it is invalid. Print an error messages to signal this
+                    cout << "INVALID INPUT - NUMBERS ARE NOT 1 OR 0" << endl;
+                    cout << endl;
+                    return 999; // return error code           	}
+                }
+            }
+
+             i++; // add one to the counter variable to move to the next element in the string
+
+        }
+
+        reverse(binary.begin(), binary.end()); // format the binary so that our answer is correct
+
+        // initialise variables for conversion algorithm
+        // Conversion algorithm was aquired from https://www.geeksforgeeks.org/program-binary-decimal-conversion/
+        int temp = stoi(binary); // note we convert the user's string to an integer using the stoi function
+        int convertedDecimal = 0;
+        int base = 1;
+        int lastNum;
+
+        // while loop runs until the temporary varible reaches 0
+        while (temp != 0)
+        {
+            // get the last number in the binary sequence
+            lastNum = temp % 10;
+
+            // move the temporary variable down to the next number in the sequence
+            temp = temp / 10;
+
+            // add the result of the last number * base to the convert variable
+            convertedDecimal += lastNum * base;
+
+            // increment the base up to the next base of 8 bit binary, the final base being 128
+            base = base * 2;
+        }
+        // end of conversion algorithm
+
+        return convertedDecimal; // return the converted decimal number
+}
+
+string decimalToBinary(int decimalNum)
+{
+    // initialise variables for conversion algorithm
+    string convertedBinary;
+    int temp = decimalNum;
+    int binaryVal;
+
+    // while loop runs 8 times to allow return value to be in 8 bit binary form
+    while (temp!=0)
+    {
+        binaryVal = temp % 2; // get the binary value be getting the remainder of decimal number / 2
+        temp = temp / 2;      // divide the decimal number by 2
+
+        if (binaryVal == 0) // check if the binary value is 0
+        {
+            convertedBinary += "0"; // if so, add a 0 to the binary string
+        }
+        else if (binaryVal == 1) // check if the binary value is 1
+        {
+            convertedBinary += "1"; // if so, add a 1 to the binary string
+        }
+    }
+
+    return convertedBinary; // return the converted binary as a string
+}
