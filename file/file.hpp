946211d68a4a3b1d379392a762a58045e9b620f4
Max Kelly <maxwill.kelly@gmail.com>
2019-11-22 13:43:31 +0000

    Implements error system in files

--- file/file.hpp
+++ file/file.hpp
@@ -3,11 +3,5 @@
 #include <string>
 #include <vector>
 
-enum states {
-  SUCCESS,
-  FILE_NOT_FOUND,
-  FILE_INVALID,
-};
-
-int loadFile(std::vector<std::string>& fileLines, std::string filename);
-int openFile(std::ifstream* fileToOpen, std::string filename);
+void loadFile(std::vector<std::string> &fileLines, std::string filename);
+int openFile(std::ifstream *fileToOpen, std::string filename);
