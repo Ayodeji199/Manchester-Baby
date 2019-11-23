564c5ddeedeb76f605d00c04af12b95c612878e6
Max Kelly <maxwill.kelly@gmail.com>
2019-11-23 00:50:00 +0000

    Fixes file not found infinite loop issue

--- error/error.hpp
+++ error/error.hpp
@@ -7,23 +7,23 @@
 // Everything is awesome
 #define SUCCESS 0
 // Pointer is unexpectedly null
-#define UNEXPECTED_NULL_POINTER -1
+#define UNEXPECTED_NULL_POINTER 1
 // A parameter to a function is invalid
-#define INVALID_INPUT_PARAMETER -2
+#define INVALID_INPUT_PARAMETER 2
 
 /*=============================================
     Application Specific Codes
 =============================================*/
 // Saving and loading file errors
-#define INVALID_FILE -100
-#define INVALID_FILENAME -101
-#define FILE_NOT_FOUND -102
-#define FILE_NOT_SAVED -103
+#define INVALID_FILE 100
+#define INVALID_FILENAME 101
+#define FILE_NOT_FOUND 102
+#define FILE_NOT_SAVED 103
 
 // Assembler specific errors
-#define INVALID_OPCODE -200
-#define INVALID_OPERAND -201
-#define INVALID_MEMORY_SIZE -202
+#define INVALID_OPCODE 200
+#define INVALID_OPERAND 201
+#define INVALID_MEMORY_SIZE 202
 
 // Displays an error message to the user based on the apppropriate error code
 // imported and quits the program, will do nothing if SUCCESS is passed
