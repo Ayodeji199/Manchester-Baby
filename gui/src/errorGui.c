425fd1c5576079c900d56a2fdf25fdc1f035f4b2
Max Kelly <maxwill.kelly@gmail.com>
2019-11-28 21:49:48 +0000

    Correct error box issue

--- gui/src/errorGui.c
+++ gui/src/errorGui.c
@@ -2,6 +2,7 @@
 #include "errorGui.h"
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <unistd.h>
 
 extern "C"
 {
@@ -10,16 +11,20 @@ extern "C"
 
     void errorBox()
     {
-        builder = gtk_builder_new_from_file("glade/errGeneric.glade");
-        dialog = GTK_WIDGET(gtk_builder_get_object(builder, "errGeneric"));
-        gtk_builder_connect_signals(builder, NULL);
-        gtk_widget_show_all(dialog);
-        g_object_unref(G_OBJECT(builder));
+        if (dialog == NULL)
+        {
+            builder = gtk_builder_new_from_file("glade/errGeneric.glade");
+            dialog = GTK_WIDGET(gtk_builder_get_object(builder, "errGeneric"));
+            gtk_builder_connect_signals(builder, NULL);
+            gtk_widget_show_all(dialog);
+            g_object_unref(G_OBJECT(builder));
+        }
     }
 
-    void on_errGeneric_response() {
-        printf("Dog");
+    void on_btnExit_clicked()
+    {
         gtk_widget_destroy(dialog);
+        dialog = NULL;
     }
 }
 
@@ -36,61 +41,46 @@ void checkValidity(int valid)
     {
     case UNEXPECTED_NULL_POINTER:
         errorBox();
-        // cout << "Function has been called using a null pointer";
+        printf("Function has been called using a null pointer");
         break;
 
     case INVALID_FILE:
         errorBox();
-        // cout << "A file has attempted to be imported that is invalid";
+        printf("A file has attempted to be imported that is invalid");
         break;
 
     case INVALID_FILENAME:
         errorBox();
-        // cout << "The filename passed as a parameter is invalid while "
-        //         "attempting to save or load a file";
+        printf("The filename passed as a parameter is invalid while attempting to save or load a file");
         break;
 
     case FILE_NOT_FOUND:
         errorBox();
-        // cout << "A file has attempted to be imported that doesn't exist";
+        printf("A file has attempted to be imported that doesn't exist");
         break;
 
     case FILE_NOT_SAVED:
         errorBox();
-        // cout << "A file cannot be saved in the location provided, this "
-        //         "could be an invalid filename or there may be insufficient "
-        //         "permissions for access";
+        printf("A file cannot be saved in the location provided, this could be an invalid filename or there may be insufficient permissions for access");
         break;
 
     case INVALID_INPUT_PARAMETER:
         errorBox();
-        // cout << "Invalid Input detected. Please try again ";
+        printf("Invalid Input detected. Please try again");
         break;
 
     case INVALID_MEMORY_SIZE:
         errorBox();
-        // cout << "Invalid or unsupported memory size input detected. "
-        //         "This could be caused by the input not being an integer "
-        //         "or the input size being invalid/unsupported";
+        printf("Invalid or unsupported memory size input detected.");
         break;
 
-    case INVALID_NUMBER_OF_ARGS: // REVIEW - there might be too many endlines here, not sure.
+    case INVALID_NUMBER_OF_ARGS:
         errorBox();
-        // cout << "Invalid number of arguments when executing the assembler. "
-        //         "Please make sure to enter the argument type, then value."
-        //      << endl
-        //      << "Arguments: [-memsize [INTEGER]] [-readname [STRING]] [-writename [STRING]] [-extended [BOOLEAN(true/false/1/0)]]"
-        //      << endl
-        //      << "Examples:" << endl
-        //      << "./assembler -memsize 64 -extended true" << endl
-        //      << "./assembler" << endl
-        //      << "./assembler -extended 0 -writename yourwritename.txt -readname yourreadname.txt -memsize 64";
+        printf("Invalid number of arguments when executing the assembler.");
         break;
-        default:
+    default:
         errorBox();
-        // cout << "Error Code is invalid or has not been fully implemented";
+        printf("Error Code is invalid or has not been fully implemented");
     }
-
-    // cout << endl;
+    printf("\n");
 }
-// }
--- gui/src/main.c
+++ gui/src/main.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include "../../assembler/assembler.hpp"
+#include <stdio.h>
 
 // Created using this tutorial
 // https://prognotes.net/2015/06/gtk-3-c-program-using-glade-3/
@@ -8,8 +9,8 @@ extern "C"
 {
     int argNum;
     char **argV;
-    GtkWidget *gfileChooserImport;
-    GtkWidget *gfileChooserExport;
+    GtkFileChooserButton *gfileChooserImport;
+    GtkFileChooserButton *gfileChooserExport;
     GtkWidget *gspinBtnMemory;
 
     int main(int argc, char *argv[])
@@ -21,14 +22,13 @@ extern "C"
         GtkWidget *window;
 
         gtk_init(&argc, &argv);
-
-        builder = gtk_builder_new_from_file("glade/windowMain.glade");
+        builder = gtk_builder_new_from_file("gui/glade/windowMain.glade");
 
         window = GTK_WIDGET(gtk_builder_get_object(builder, "windowMain"));
         gtk_builder_connect_signals(builder, NULL);
 
-        gfileChooserImport = GTK_WIDGET(gtk_builder_get_object(builder, "fileChooserImport"));
-        gfileChooserExport = GTK_WIDGET(gtk_builder_get_object(builder, "fileChooserExport"));
+        gfileChooserImport = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "fileChooserImport"));
+        gfileChooserExport = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "fileChooserExport"));
         gspinBtnMemory = GTK_WIDGET(gtk_builder_get_object(builder, "spinBtnMemory"));
 
         g_object_unref(builder);
@@ -37,6 +37,12 @@ extern "C"
         gtk_main();
 
         return 0;
+    }
+
+    // Called when a file to import is set by the user
+    void on_fileChooserBtnImport_current_folder_changed(GtkFileChooserButton *fileChooserBtnImport, gpointer user_data)
+    {
+        printf("Import File attempted");
     }
 
     // Called when window is closed
@@ -49,12 +55,12 @@ extern "C"
     void on_btnAssembler_clicked()
     {
         Assembler assemblerObj = Assembler();
-        // assemblerObj.assignArgs(assemblerObj.getArgs(argNum, argV));
+        assemblerObj.assignArgs(assemblerObj.getArgs(argNum, argV));
         std::string memsize = std::to_string(gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(gspinBtnMemory)));
-        // std::string readme = std::to_string(GtkFileChooser(gfileChooserImport)->get_filename());
-        // std::string writename = std::to_string(gtk_file_chooser_button_get_title(GtkFileChooserButton(gfileChooserExport)));
-        // std::vector<std::string> run_args = {"-memsize ", memsize, "-readname", readme, "-writename", writename};
-        // assemblerObj.assignArgs(run_args);
+        char *readname = gtk_file_chooser_get_filename((GTK_FILE_CHOOSER(gfileChooserImport)));
+        char *writename = gtk_file_chooser_get_filename((GTK_FILE_CHOOSER(gfileChooserExport)));
+        std::vector<std::string> run_args = {"-memsize ", memsize, "-readname", std::string(readname), "-writename", std::string(writename)};
+        assemblerObj.assignArgs(run_args);
         assemblerObj.assembly();
     }
 
