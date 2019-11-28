ca5b7571c6a6e97e1bb04a30d592358961c55442
Nick <noodles524@gmail.com>
2019-11-28 16:06:55 +0000

    Started working on conversion methods to account for two's complement

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -12,24 +12,42 @@
 #include <stdlib.h> 
 using namespace std;
 // This is the array that holds the values of two to the 32nd power starting from 1
-long long  Bits[32];
-/**
- * This is the method that takes in the int,
- *  multiplies it by the exponent of its 
- * current position then adds them together
- */
+long long Bits[32];
+
+/*
+  This is the method that takes in the int,
+  multiplies it by the exponent of its 
+  current position then adds them together
+*/
 long long test(long long val[] , int length)
 {
     long long m = 0;
-    for (int i = 0; i < length; i++)
+    int limit;
+    bool negative;
+    
+    if(val[length-1]==1)
+    {
+        limit = length-1;
+        negative = true;
+    } else {
+        limit = length;
+        negative = false;
+    }
+
+    for (int i = 0; i < limit; i++)
     {
         m = m + (val[i] * Bits[i]);
     }
+
+    if (negative){
+        m = -m;
+    }
+
     return m;   
 }
-/**
- * This is the method that helpes to calculate the exponent in the array
- */
+/*
+  This is the method that helpes to calculate the exponent in the array
+*/
 long long exponent(int power)
 {
     long long base = 1;
@@ -72,41 +90,56 @@ long long binaryToDecimal(string bi , int length)
         {
             int ia = (int)bi[i];
             dumb[i]=ia - 48;
+            
         }
     finalval = test(dumb,length);
+
+    cout << "Converted " << bi << " to decimal: " << finalval << endl;
 
     return finalval;
 }
 
 
-
-string decimalToBinary(long long decimalNum, int bits)
+/*
+Converts decimal to binary (string)
+*/
+string decimalToBinary(long long decimalNum, int numberOfBits)
 {
     // initialise variables for conversion algorithm
     string convertedBinary;
-    long long temp = decimalNum;
+    long long temp = abs(decimalNum);
     long long binaryVal;
     int i = 0;
 
-    // while loop runs 8 times to allow return value to be in 8 bit binary form
-    while (i != bits)
+    // while loop runs 'numberOfBits' times to allow return value to be specified bit length in binary form
+    while (i < numberOfBits)
     {
         binaryVal = temp % 2; // get the binary value be getting the remainder of decimal number / 2
         temp = temp / 2;      // divide the decimal number by 2
 
-        if (binaryVal == 0) // check if the binary value is 0
-        {
-            convertedBinary += "0"; // if so, add a 0 to the binary string
-        }
-        else if (binaryVal == 1) // check if the binary value is 1
-        {
-            convertedBinary += "1"; // if so, add a 1 to the binary string
+        if (i < numberOfBits-1){ // check if i isn't last bit
+            if (binaryVal == 0) // check if the binary value is 0
+            {
+                convertedBinary += "0"; // if so, add a 0 to the binary string
+            }
+            else if (binaryVal == 1) // check if the binary value is 1
+            {
+                convertedBinary += "1"; // if so, add a 1 to the binary string
+            }
+        } else { // if i is on the last bit
+            if(decimalNum < 0){ // if the input number is negative
+                convertedBinary += "1"; // use two's complement to store it correctly
+            } else {
+                convertedBinary += "0"; // don't use two's complement so value stays positive
+            }
         }
 
         i++;
     }
 
-    cout << convertedBinary << endl;
+    // cout << convertedBinary << endl;
+
+    cout << "Converted " << decimalNum << " to binary: " << convertedBinary << endl;
 
     return convertedBinary; // return the converted binary as a string
 }
